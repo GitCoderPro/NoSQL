b261b4b1fee7108c47e963ce2b6fc970243178ea
GitCoder <149430532+GitCoderPro@users.noreply.github.com>
2023-12-28 02:04:56 +0000

    Adding enviroment variables

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const server = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://Username:Password@nosqlcluster.wxnnkwa.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.Username}:${process.env.Password}@nosqlcluster.wxnnkwa.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
